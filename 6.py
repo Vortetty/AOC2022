8185c1c9bc3bc7ee6b7989b1909a822849c6c4ca
wintersys <vortettyspam@gmail.com>
2022-12-05 22:12:12 -0800

    day 6 python

---
+++ 6.py
@@ -0,0 +1,11 @@
+with open("6.txt", "r") as f:
+    text = f.read()
+    i = 4
+    while (i := i+1):
+        if len(set(text[i-4:i])) == 4: break
+    print(f"PT.1 >> {i} ({text[i-4:i]})")
+
+    i = 14
+    while (i := i+1):
+        if len(set(text[i-14:i])) == 14: break
+    print(f"PT.2 >> {i} ({text[i-14:i]})")
